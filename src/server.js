37ef70471047cc97d41f774c92d29fdbe28843db
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-08-29 18:36:02 -0600

    backend server exoress imported and created

--- src/server.js
+++ src/server.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/hello', (req, response) => response.send("Hi!"));
 
-app.listen(8000, () => console.log('Listinig on port'));
+app.listen(8000, () => console.log('Listinig on port 8000'));
