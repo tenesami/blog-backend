3bd9883e52a7647e20694d4c7fe79359c58d47c4
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-08-30 17:40:59 -0600

    import person to add json body

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+//parse json obj include with post request  & include body property
+//for the matching route
+app.use(bodyParser.json());
+
 app.get('/hello', (req, response) => response.send("Hi!"));
 
-app.post('/hello', (req, response) => response.send("Hi!"));
+app.post('/hello', (req, response) => response.send(`Hi ${req.body.name}!`));
 
 app.listen(8000, () => console.log('Listinig on port 8000'));
