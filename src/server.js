24bc0ef55d812342b2830cbe88588089ea3d2d48
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-08-30 17:27:15 -0600

    add the post request route

--- src/server.js
+++ src/server.js
@@ -4,4 +4,6 @@ const app = express();
 
 app.get('/hello', (req, response) => response.send("Hi!"));
 
+app.post('/hello', (req, response) => response.send("Hi!"));
+
 app.listen(8000, () => console.log('Listinig on port 8000'));
