f3b2b44580accfb5ea50e0936b40bd276f8dd7d3
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-08-31 09:37:25 -0600

    adding comment to article route and fack db created

--- src/server.js
+++ src/server.js
@@ -5,12 +5,15 @@ import bodyParser from 'body-parser';
 const articlesInfo = {
     'learn-react': {
         upvotes: 0,
+        comments: [],
     },
     'learn-node': {
         upvotes: 0,
+        comments: [],
     },
     'my-thoughts-on-resumes': {
         upvotes: 0,
+        comments: [],
     },
 }
 const app = express();
@@ -30,7 +33,16 @@ app.post('/api/articles/:name/upvote', (req, res) =>{
     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
 })
 
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    //assgin or db persing properties
+    const { username, text } = req.body;
 
+    const articleName = req.params.name;
+
+     articlesInfo[articleName].comments.push({username, text});
+
+     res.status(200).send(articlesInfo[articleName]);
+})
 // app.get('/hello', (req, response) => response.send("Hi!"));
 // //Show one article based on the id on the url
 // app.get('/hello/:name', (req, response) => response.send(`Hi ${req.params.name}!`));
