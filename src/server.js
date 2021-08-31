a17468aa6e452f608e825a84a3b0c2105ecb925e
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-08-30 18:03:52 -0600

    create show route on server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -9,6 +9,10 @@ app.use(bodyParser.json());
 
 app.get('/hello', (req, response) => response.send("Hi!"));
 
+//Show one article based on the id on the url
+app.get('/hello/:name', (req, response) => response.send(`Hi ${req.params.name}!`));
+
+//create one article 
 app.post('/hello', (req, response) => response.send(`Hi ${req.body.name}!`));
 
 app.listen(8000, () => console.log('Listinig on port 8000'));
